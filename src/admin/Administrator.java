98587abf3bd8e2eb077aee828aec7e0490bfc33d
kmannarelli <kmannarelli@info-206-12x.iut.bx1>
2021-05-12 17:35:06 +0200

    Création de la base de donnée une seule et unique fois

--- src/admin/Administrator.java
+++ src/admin/Administrator.java
@@ -12,6 +12,7 @@ public class Administrator {
 	public static void createDatabase() {
 		String sqlite_driver = "org.sqlite.JDBC";
 		String path= "jdbc:sqlite:database.db";
+		boolean alreadycreated=false;
 		try {
 			Class.forName(sqlite_driver);
 			
@@ -20,11 +21,28 @@ public class Administrator {
 		}
 		try(Connection c
 				= DriverManager.getConnection(path)) {
-			
-			//Statement s = c.createStatement();
-			//String sql = "CREATE DATABASE SOKOBAN";
-			//s.executeUpdate(sql);
+			Statement s = c.createStatement();
 			DatabaseMetaData meta = c.getMetaData();
+			
+			
+			ResultSet r = s.executeQuery("select * from existboolean");
+			while(r.next()) {
+				alreadycreated = Integer.parseInt(r.getString("exist")) == 1;
+				System.out.println();
+			}
+			
+			
+			if(!alreadycreated) {
+				System.out.println("DB Created");
+				s.execute("create table board" + "(name text, row integer , col integer)");
+				s.execute("create table row" + "(name text, row_num integer , description text)");
+				s.execute("create table existboolean" + "(exist integer)");
+				s.executeUpdate("insert into existboolean " + "values(1)" );
+				
+				//s.executeUpdate("insert into board " + "values ('Board1', 6, 6)");	
+			}
+			
+			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
